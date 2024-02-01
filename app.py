3d525f01c59ddcc4a96596a39fd5b1835ee51094
enroot <enroot@debian.home>
2024-02-01 22:55:10 +0100

    use user input to display exercise specifics

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # pylint: disable=missing-module-docstring
+import ast
 
 import duckdb
 import streamlit as st
-
 
 con = duckdb.connect(database="data/exercises_sql_table.duckdb", read_only=False)
 
@@ -11,7 +11,7 @@ con = duckdb.connect(database="data/exercises_sql_table.duckdb", read_only=False
 with st.sidebar:
     theme = st.selectbox(
         "what would you like to review?",
-        ("cross_joins", "GroupBy", "Windows Functions"),
+        ("cross_joins", "GroupBy", "window_functions"),
         index=None,
         placeholder="Select a theme...",
     )
@@ -22,6 +22,10 @@ with st.sidebar:
 
 st.header("enter your code: ")
 query = st.text_area(label="votre code SQL ici", key="user_input")
+
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
 
 # if query:
 #     result = duckdb.sql(query).df()
@@ -43,15 +47,18 @@ query = st.text_area(label="votre code SQL ici", key="user_input")
 #         )
 #
 #
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
-#
-# with tab2:
-#     st.write("table: beverages")
-#     st.dataframe(beverages)
-#     st.write("table: food_items")
-#     st.dataframe(food_items)
-#     st.write("expected: ")
-#     st.dataframe(solution_df)
-#
-# with tab3:
-#     st.write(ANSWER_STR)
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
+
+with tab3:
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r", encoding="utf-8") as f:
+        answer = f.read
+    st.text(answer)
